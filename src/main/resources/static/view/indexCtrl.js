78629330759c7bd4cb7619d16530b6b2b2fe008f
liu811585982 <811585982@qq.com>
2017-07-02 13:45:21 +0800

    登录注销

--- src/main/resources/static/view/indexCtrl.js
+++ src/main/resources/static/view/indexCtrl.js
@@ -1,13 +1,13 @@
-define(['../script/tms', 'jquery'], function(module, $){
+define(['../script/tms', 'jquery', '../script/service/loginService'], function(module, $, LoginService){
     module.controller('indexCtrl', function($rootScope, $scope, $resource, $location, $route, commonService){
         var _this = this;
 
-        /*var loginCookie = commonService.getCookie('token');
+        var loginCookie = commonService.getCookie('staff_token');
         if(!loginCookie){
             window.location.href = '/login';
         }else{
             $rootScope.loginCookie = loginCookie;
-        }*/
+        }
 
         //监控浏览器地址栏变化，如果变化刷新页面。解决点击浏览器的回退，前进按钮页面不刷新的问题
         $rootScope.$watch(function () {return $location.path()}, function (newLocation, oldLocation) {
@@ -26,8 +26,6 @@ define(['../script/tms', 'jquery'], function(module, $){
         this.activeMenu = function(index){
             $rootScope.activeIndex = index;
         };
-        console.log($location.url());
-        console.log(123);
 
         var url = $location.url();
         this.activeMenuByRoute = function(){
@@ -39,5 +37,30 @@ define(['../script/tms', 'jquery'], function(module, $){
         }
 
         this.activeMenuByRoute();
+
+        var loginService = new LoginService($resource);
+        //登出
+        this.logout = function(){
+            console.log(13);
+            loginService.logout(function(data){
+                if(data.status == 'false'){
+                    commonService.deleteCookie('staff_token');
+                }
+                window.location.href = '/login';
+            });
+        };
+
+        //获取用户信息
+        this.getLoginUser = function(){
+            loginService.loginToken(function(data){
+                if(data.status == 'true'){
+                    $rootScope.loginUser = data.message;
+                }else{
+                    console.log(data);
+                }
+            });
+        };
+
+        this.getLoginUser();
     });
 });
