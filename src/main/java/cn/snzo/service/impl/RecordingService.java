65582fc2ac013a8aaaf2b826dd45ebd793563fc7
sdnb <xgct0505@163.com>
2017-07-04 23:27:52 +0800

    录音api修改

--- src/main/java/cn/snzo/service/impl/RecordingService.java
+++ src/main/java/cn/snzo/service/impl/RecordingService.java
@@ -4,14 +4,18 @@ import cn.snzo.common.CommonUtils;
 import cn.snzo.entity.Recording;
 import cn.snzo.repository.RecordingRepository;
 import cn.snzo.service.IRecordingService;
+import cn.snzo.utils.BeanUtil;
 import cn.snzo.utils.PageUtil;
 import cn.snzo.vo.RecordingShow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -35,6 +39,12 @@ public class RecordingService implements IRecordingService {
         Pageable p = PageUtil.createPage(currentPage, pageSize);
         filename = CommonUtils.fuzzyString(filename);
         Page<Recording> recordings = recordingRepository.findPage(filename, createStart, createEnd, p);
-        return PageUtil.entityToShowPage(recordings, p);
+        List<RecordingShow> recordingShows  = new ArrayList<>();
+        for (Recording r : recordings) {
+            RecordingShow show = new RecordingShow();
+            BeanUtil.entityToShow(r, show);
+            recordingShows.add(show);
+        }
+        return new PageImpl<RecordingShow>(recordingShows, p, recordings.getTotalElements());
     }
 }
