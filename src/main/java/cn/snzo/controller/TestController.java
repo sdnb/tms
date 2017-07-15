285259e1fd3b9895f3466883aa13d8fe80cb1db0
chentao <xgct0505@qq.com>
2017-07-15 13:18:37 +0800

    修改

--- src/main/java/cn/snzo/controller/TestController.java
+++ src/main/java/cn/snzo/controller/TestController.java
@@ -1,5 +1,7 @@
 package cn.snzo.controller;
 
+import cn.snzo.service.IConductorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-
+    @Autowired
+    private IConductorService conductorService;
     @RequestMapping("/hwe")
     public String twe(){
+        conductorService.getOne(1);
         return "Hwe";
     }
 }
