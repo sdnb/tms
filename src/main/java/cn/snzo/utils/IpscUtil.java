36699fe092f4f195ccbb0fc7f9054912220908dc
chentao <xgct0505@qq.com>
2017-07-16 22:28:57 +0800

    log

--- src/main/java/cn/snzo/utils/IpscUtil.java
+++ src/main/java/cn/snzo/utils/IpscUtil.java
@@ -196,6 +196,7 @@ public class IpscUtil {
             params.put("to_uri", te.getPhone()+"@"+ip); /// 被叫号码的 SIP URI
             params.put("max_answer_seconds", Constants.MAX_ANSWER_SECONDS); /// 该呼叫最长通话允许时间
             params.put("user_data", te.getPhone()+"-"+te.getName()); ///用户信息
+            logger.info("呼叫参数： {}", params);
             commander.createResource(
                     busAddress,
                     "sys.call",
