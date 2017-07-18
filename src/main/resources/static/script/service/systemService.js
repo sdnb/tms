4f905abc6404652bd3919a938f8d43dc9d7cfb87
liu811585982 <811585982@qq.com>
2017-07-18 23:39:02 +0800

    系统设置

--- src/main/resources/static/script/service/systemService.js
+++ src/main/resources/static/script/service/systemService.js
@@ -2,6 +2,9 @@ define([],function(){
     function SystemService($resource){
         this.logFindPageApi = $resource('/api/log/page'); //分页查询日志
         this.updatePwdApi = $resource('/api/setting/pwd',null,{update:{method:'PUT'}});
+        this.getSettingApi = $resource('/api/setting'); //获取系统设置
+        this.addSettingApi = $resource('/api/setting'); //新增系统设置
+        this.updateSettingApi = $resource('/api/setting/:id',null,{update:{method:'PUT'}});
     }
 
     SystemService.prototype.logFindPage = function(filter,cb){
@@ -12,6 +15,30 @@ define([],function(){
 
     SystemService.prototype.updatePwd = function(account,cb){
         this.updatePwdApi.update(account,function(data){
+            cb(data);
+        },function(errData){
+            cb(errData.data.error);
+        });
+    };
+
+    SystemService.prototype.getSetting = function(cb){
+        this.getSettingApi.get(function(data){
+            cb(data);
+        },function(errData){
+            cb(errData.data.error);
+        });
+    };
+
+    SystemService.prototype.addSetting = function(setting,cb){
+        this.addSettingApi.save(setting,function(data){
+            cb(data);
+        },function(errData){
+            cb(errData.data.error);
+        });
+    };
+
+    SystemService.prototype.updateSetting = function(id,setting,cb){
+        this.updateSettingApi.update({id:id},setting,function(data){
             cb(data);
         },function(errData){
             cb(errData.data.error);
--- src/main/resources/static/view/xt/ljCtrl.js
+++ src/main/resources/static/view/xt/ljCtrl.js
@@ -1,5 +1,88 @@
-define(['../../script/tms', 'jquery'], function(module, $){
-    module.controller('ljCtrl', function($scope, $location){
-        console.log('ljCtrl');
+define(['../../script/tms', 'jquery', '../../script/service/loginService', '../../script/service/systemService'],
+    function(module, $, LoginService, SystemService){
+    module.controller('ljCtrl', function($scope, $location, $resource, commonService){
+        var loginCookie = commonService.getCookie('staff_token');
+        if(loginCookie == ''){
+            window.location.href = '/login';
+        }
+
+        var _this = this;
+        this.isNull = false;
+        this.loading = false;
+        this.message = {
+            show: false,
+            text: null
+        };
+
+        this.hideMessage = function(){
+            this.message.show = false;
+        };
+
+        var loginService = new LoginService($resource);
+        var systemService = new SystemService($resource);
+
+        //获取当前登录人
+        this.getLoginUser = function(){
+            loginService.loginToken(function(data){
+                if(data.status == 'true'){
+                    $scope.loginUser = data.message;
+                }else{
+                    console.log(data);
+                }
+            });
+        };
+        this.getLoginUser();
+
+        this.setting = null;
+        this.getCurrentSetting = function(){
+            systemService.getSetting(function(data){
+                if(data.status == 'true'){
+                    _this.setting = data.message;
+                }else{
+                    _this.setting = null;
+                    console.log(data);
+                }
+            });
+        };
+        this.getCurrentSetting();
+
+        this.portPattern = commonService.regex('positiveInteger');
+
+        this.saveSetting = function(flag){
+            this.isNull = true;
+            if(!flag) return;
+            this.loading = true;
+            systemService.addSetting(this.setting,function(data){
+                _this.loading = false;
+                _this.message.show = true;
+                _this.message.text = data.message;
+                if(data.status == 'true'){
+                    _this.getCurrentSetting();
+                }else{
+                    console.log(data);
+                }
+            });
+        };
+
+        this.updateCurrentSetting = function(flag){
+            this.isNull = true;
+            if(!flag) return;
+            this.loading = true;
+            systemService.updateSetting(this.setting.id,this.setting,function(data){
+                _this.loading = false;
+                _this.message.show = true;
+                _this.message.text = data.message;
+                if(data.status == 'true'){
+                    _this.getCurrentSetting();
+                }else{
+                    console.log(data);
+                }
+            });
+        };
+
+        this.reset = function(){
+            this.isNull = false;
+            this.getCurrentSetting();
+        };
     });
 });
