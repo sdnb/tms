aab32bbdea534cc7868acc553162e1d6d428c30f
sdnb <xgct0505@163.com>
2017-07-30 18:25:55 +0800

    修改

--- src/main/resources/static/view/hyCtrl.js
+++ src/main/resources/static/view/hyCtrl.js
@@ -518,7 +518,7 @@ define(['../script/tms', 'jquery','../script/service/loginService','../script/se
             $rootScope.socket.onmessage = function(even){
                 _this.confResId = event.data;
                 if(_this.conference != undefined && _this.conference.resId == _this.confResId){
-                    _this.getMembers('reload');
+                    //_this.getMembers('reload');
                     _this.getConferences(_this.conductor.id);
                 }
             };
