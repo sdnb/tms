5b082a045c8f68ea4cde18c1ecf2a0747a2e917b
sdnb <xgct0505@163.com>
2017-08-06 22:10:01 +0800

    调试

--- src/main/java/cn/snzo/controller/TestController.java
+++ src/main/java/cn/snzo/controller/TestController.java
@@ -5,13 +5,13 @@ import cn.snzo.repository.CallRepository;
 import cn.snzo.repository.ConductorRepository;
 import cn.snzo.repository.ContactRepository;
 import cn.snzo.service.IConductorService;
+import cn.snzo.service.IpscService;
+import cn.snzo.utils.IpscUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by ThomasC on 2017/6/29 0029.
@@ -31,12 +31,16 @@ public class TestController {
     @Autowired
     private CallRepository callRepository;
 
+    @Autowired
+    private IpscService ipscService;
+
     @RequestMapping("/hwe")
-    public Object twe(){
-        List<Integer> status = new ArrayList<>();
-        status.add(1);
-        status.add(2);
-        callRepository.findCallByConfResIdAndStatus(null, status);
+    public Object twe(@RequestParam(required = false)String confResId){
+        IpscUtil.playConfVoice(confResId, Constants.COME_IN_TICK);
+//        List<Integer> status = new ArrayList<>();
+//        status.add(1);
+//        status.add(2);
+//        callRepository.findCallByConfResIdAndStatus(null, status);
         return Constants.VOICE_PATH;
     }
 
